ffae88e29680da752bc657641f16857db27e9a73
Iren <iren@gmail.com>
2019-10-17 12:36:07 +0200

    add name of class to BAM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -409,32 +409,32 @@ function moveRange (elem) {
         document.removeEventListener('touchmove', onMouseMove);
     }}
 
-    $(document).on('click', ".carousel-button-right",function(){
-                var carusel = $(this).parents('.carousel');
+    $(document).on('click', ".cards-img__button-right",function(){
+                var carusel = $(this).parents('.cards-img__carousel');
                 right_carusel(carusel);
                 return false;
             });
 
 
-            $(document).on('click',".carousel-button-left",function(){
-                var carusel = $(this).parents('.carousel');
+            $(document).on('click',".cards-img__button-left",function(){
+                var carusel = $(this).parents('.cards-img__carousel');
                 left_carusel(carusel);
                 return false;
             });
             function left_carusel(carusel){
-                var block_width = $(carusel).find('.carousel-block').outerWidth();
-                $(carusel).find(".carousel-items .carousel-block").eq(-1).clone().prependTo($(carusel).find(".carousel-items"));
-                $(carusel).find(".carousel-items").css({"left":"-"+block_width+"px"});
-                $(carusel).find(".carousel-items .carousel-block").eq(-1).remove();
-                $(carusel).find(".carousel-items").animate({left: "0px"}, 200);
+                var block_width = $(carusel).find('.cards-img__carousel-block').outerWidth();
+                $(carusel).find(".cards-img__carousel-items .cards-img__carousel-block").eq(-1).clone().prependTo($(carusel).find(".cards-img__carousel-items"));
+                $(carusel).find(".cards-img__carousel-items").css({"left":"-"+block_width+"px"});
+                $(carusel).find(".cards-img__carousel-items .cards-img__carousel-block").eq(-1).remove();
+                $(carusel).find(".cards-img__carousel-items").animate({left: "0px"}, 200);
 
             }
             function right_carusel(carusel){
-                var block_width = $(carusel).find('.carousel-block').outerWidth();
-                $(carusel).find(".carousel-items").animate({left: "-"+ block_width +"px"}, 200, function(){
-                    $(carusel).find(".carousel-items .carousel-block").eq(0).clone().appendTo($(carusel).find(".carousel-items"));
-                    $(carusel).find(".carousel-items .carousel-block").eq(0).remove();
-                    $(carusel).find(".carousel-items").css({"left":"0px"});
+                var block_width = $(carusel).find('.cards-img__carousel-block').outerWidth();
+                $(carusel).find(".cards-img__carousel-items").animate({left: "-"+ block_width +"px"}, 200, function(){
+                    $(carusel).find(".cards-img__carousel-items .cards-img__carousel-block").eq(0).clone().appendTo($(carusel).find(".cards-img__carousel-items"));
+                    $(carusel).find(".cards-img__carousel-items .cards-img__carousel-block").eq(0).remove();
+                    $(carusel).find(".cards-img__carousel-items").css({"left":"0px"});
                 });
             }
 
